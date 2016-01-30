d412661b880672554cce1e9e79002e954d1a8bb5
nardavin <nardavin@gmail.com>
2016-01-29 18:38:13 -0700

    First commit - set up Robot.java framework

---
+++ src/org/usfirst/frc/team2403/robot/Robot.java
@@ -0,0 +1,45 @@
+
+package org.usfirst.frc.team2403.robot;
+
+import edu.wpi.first.wpilibj.*;
+
+public class Robot extends IterativeRobot {
+
+	/**
+	 * Initialization for robot - called once when robot turns on
+	 * 
+	 * @author Nic A
+	 */
+    public void robotInit() {
+    	
+    }
+    
+    /**
+	 * Initialization for auto mode - called once when robot starts auto mode
+	 * 
+	 * @author Nic A
+	 */
+    public void autonomousInit() {
+    	
+    }
+    
+    /**
+	 * Main loop for auto mode - called once every 20ms while in auto mode
+	 * 
+	 * @author Nic A
+	 */
+    public void autonomousPeriodic() {
+    	
+    }
+    
+    /**
+	 * Main loop for teleop mode - called once every 20ms while in teleop mode
+	 * 
+	 * @author Nic A
+	 */
+    public void teleopPeriodic() {
+        
+    }
+    
+    
+}
